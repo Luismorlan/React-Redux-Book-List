3eaada5ff941ed9abac2fcb6303899da0a7eacb1
Weilun Chen <warren@Weiluns-MacBook-Pro.local>
2017-02-28 13:39:01 -0800

    Finished the project of book list using react-redux

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function () {
     return [
-        {title:'JS'},
-        {title:'Harry Potter'},
-        {title:'The Dark Tower'},
-        {title:'Eloquent Ruby'}
+        {title:'JS', pages: 101},
+        {title:'Harry Potter', pages: 103},
+        {title:'The Dark Tower', pages: 79},
+        {title:'Eloquent Ruby', pages: 1}
     ];
 }
